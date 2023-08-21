ca3c67b66cc1e9f2f635dd68e67be292076f69c3
WatchJani <jankokondic84@gmail.com>
2023-08-21 21:09:38 +0200

    update

--- model/user/sql/user_query.go
+++ model/user/sql/user_query.go
@@ -5,15 +5,9 @@ func GetAll() string {
 }
 
 func GetUser() string {
-	return `SELECT
-    *
-FROM
-    team.users
-WHERE	
-    id = $1;`
+	return `SELECT * FROM team.users WHERE id = $1;`
 }
 
 func NewUser() string {
-	return `INSERT INTO team.users (name, lastname, password, phone, mail, username, borndate)
-    VALUES ($1, $2, $3, $4, $5, $6, $7)`
+	return `INSERT INTO team.users (name, lastname, password, phone, mail, username, borndate) VALUES ($1, $2, $3, $4, $5, $6, $7)`
 }
